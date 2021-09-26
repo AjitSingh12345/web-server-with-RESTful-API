bdb4279204311cdf957881719895faac42eef047
AjitSingh12345 <asingh0525@gmail.com>
2021-09-26 12:16:03 -0500

    updates

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/purchase', function(req, res) {
            res.status(500).end()
        } else {
            const itemsJson = JSON.parse(data)
-           const itemsArray = itemsJson.music.concat(itemsJson.merch)
+           const itemsArray = itemsJson.Products.concat(itemsJson.Clothes)
            let total = 0
            req.body.items.forEach(function(item) {
                const itemJson = itemsArray.find(function(i) {
